cde6ae29001ccd6544ac11a97b9267ffde367831
Eric Vaandering <ewv@fnal.gov>
2026-03-05 10:35:48 -0600

    Update delivery.json

--- containers/Analysis/build_images.sh
+++ containers/Analysis/build_images.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 #cd ~/solar-monitoring/
-podman build . -f containers/Analysis/Containerfile -t ericvaandering/solar-analysis:0.2.0
-podman push ericvaandering/solar-analysis:0.2.0
+podman build . -f containers/Analysis/Containerfile -t ericvaandering/solar-analysis:0.3.0
+podman push ericvaandering/solar-analysis:0.3.0
