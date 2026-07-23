b3bc9a3a2e26c0c719e9082d5a01e181c3d4f022
Eric Vaandering <ewv@fnal.gov>
2026-07-23 11:13:40 -0500

    Bump version

--- containers/Analysis/build_images.sh
+++ containers/Analysis/build_images.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 #cd ~/solar-monitoring/
-podman build . -f containers/Analysis/Containerfile -t ericvaandering/solar-analysis:1.0.0
-podman push ericvaandering/solar-analysis:1.1.0
+podman build . -f containers/Analysis/Containerfile -t ericvaandering/solar-analysis:1.1.1
+podman push ericvaandering/solar-analysis:1.1.1
